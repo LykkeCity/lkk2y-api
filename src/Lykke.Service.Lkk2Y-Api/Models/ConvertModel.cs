bae92b2cdf39f98d7093f9e008276a70bfb38f7c
andrey.migin@lykkex.com <amigin@gmail.com>
2017-11-28 23:48:20 +0200

    fixed convert model

--- src/Lykke.Service.Lkk2Y-Api/Models/ConvertModel.cs
+++ src/Lykke.Service.Lkk2Y-Api/Models/ConvertModel.cs
@@ -4,19 +4,15 @@ namespace Lykke.Service.Lkk2Y_Api.Models
 {
     public class ConvertModel
     {
-        public class AssetFromModel{
-            public string AssetId { get; set; }
-            public double Amount { get; set; }
-        }
 
-        [JsonProperty("baseAssetId")] 
-        public string BaseAssetId { get; set; }
+        [JsonProperty("from")] 
+        public string From { get; set; }
         
-        [JsonProperty("orderAction")] 
-        public string OrderAction { get; set; }
+        [JsonProperty("amount")] 
+        public double Amount { get; set; }
 
-        [JsonProperty("assetsFrom")] 
-        public AssetFromModel AssetFrom { get; set; }
+        [JsonProperty("to")] 
+        public string To { get; set; }
     }
-    
+
 }
