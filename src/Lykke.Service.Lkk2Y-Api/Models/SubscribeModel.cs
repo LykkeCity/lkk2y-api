70d2f9f67eb4c9e22dbe2ab872c35efdd2d5b99d
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-11-28 17:37:32 +0300

    Restored ValuesController and fixed project default namespaces

---
+++ src/Lykke.Service.Lkk2Y-Api/Models/SubscribeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.Lkk2Y_Api.Models
+{
+    public class SubscribeModel
+    {
+        public string Email { get; set; }
+    }
+}
