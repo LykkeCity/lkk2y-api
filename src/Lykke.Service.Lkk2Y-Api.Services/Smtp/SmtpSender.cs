a5d71790aa941c9c7c03c97f4e62aaf9e33b95a4
andrey.migin@lykkex.com <amigin@gmail.com>
2017-12-05 01:17:45 +0200

    Invalid email should not break subscription

--- src/Lykke.Service.Lkk2Y-Api.Services/Smtp/SmtpSender.cs
+++ src/Lykke.Service.Lkk2Y-Api.Services/Smtp/SmtpSender.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Lykke.Service.Lkk2Y_Api.Core.Smtp;
 using MailKit.Net.Smtp;
 using MailKit.Security;
@@ -23,26 +24,33 @@ namespace Lykke.Service.Lkk2Y_Api.Services.Smtp
         
         public async Task SendEmailAsync(string email, string subject, string body)
         {
-            var emailMessage = new MimeMessage();
-
-            emailMessage.From.Add(new MailboxAddress(_smtpSenderSettings.FromDisplayName, _smtpSenderSettings.From));
-            emailMessage.To.Add(new MailboxAddress("", email));
-            emailMessage.Subject = subject;
-
-            var bodyPart = new TextPart("html") { Text = body };
-
-            emailMessage.Body = bodyPart;
-
-
-            using (var client = new SmtpClient())
+            try
             {
-                client.LocalDomain = _smtpSenderSettings.Domain;
+                var emailMessage = new MimeMessage();
 
-                await client.ConnectAsync(_smtpSenderSettings.Host, _smtpSenderSettings.Port, GetSecureSocketOptions()).ConfigureAwait(false);
-                await client.AuthenticateAsync(_smtpSenderSettings.Login, _smtpSenderSettings.Password);
+                emailMessage.From.Add(new MailboxAddress(_smtpSenderSettings.FromDisplayName, _smtpSenderSettings.From));
+                emailMessage.To.Add(new MailboxAddress("", email));
+                emailMessage.Subject = subject;
 
-                await client.SendAsync(emailMessage).ConfigureAwait(false);
-                await client.DisconnectAsync(true).ConfigureAwait(false);
+                var bodyPart = new TextPart("html") { Text = body };
+
+                emailMessage.Body = bodyPart;
+
+
+                using (var client = new SmtpClient())
+                {
+                    client.LocalDomain = _smtpSenderSettings.Domain;
+
+                    await client.ConnectAsync(_smtpSenderSettings.Host, _smtpSenderSettings.Port, GetSecureSocketOptions()).ConfigureAwait(false);
+                    await client.AuthenticateAsync(_smtpSenderSettings.Login, _smtpSenderSettings.Password);
+
+                    await client.SendAsync(emailMessage).ConfigureAwait(false);
+                    await client.DisconnectAsync(true).ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
             
         }
