a99e5c85e14be9ddd931efaa9a6242e501c9b0ae
andrey.migin@lykkex.com <amigin@gmail.com>
2017-12-09 14:43:06 +0200

    Added Blacklist Repositories

---
+++ src/Lykke.Service.Lkk2Y-Api.AzureRepositories/BlacklistedEmails.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using AzureStorage;
+using Lykke.Service.Lkk2Y_Api.Core;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.Service.Lkk2Y_Api.AzureRepositories
+{
+    public class BlacklistedEmailEntity : TableEntity
+    {
+        public static string GeneratePartitionKey()
+        {
+            return "b";
+        }
+
+        public static string GenerateRowKey(string email)
+        {
+            return email.ToLower();
+        }
+        
+    }
+    
+    public class BlacklistedEmails : IBlacklistedEmails
+    {
+        private readonly INoSQLTableStorage<BlacklistedEmailEntity> _tableStorage;
+
+        public BlacklistedEmails(INoSQLTableStorage<BlacklistedEmailEntity> tableStorage)
+        {
+            _tableStorage = tableStorage;
+        }
+        
+        public async Task<bool> IsBlacklistedAsync(string email)
+        {
+            var partitionKey = BlacklistedEmailEntity.GeneratePartitionKey();
+            var rowKey = BlacklistedEmailEntity.GenerateRowKey(email);
+
+            return await _tableStorage.GetDataAsync(partitionKey, rowKey) != null;
+        }
+    }
+}
---
+++ src/Lykke.Service.Lkk2Y-Api.Core/IBlacklistedEmails.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Service.Lkk2Y_Api.Core
+{
+    public interface IBlacklistedEmails
+    {
+        Task<bool> IsBlacklistedAsync(string email);
+    }
+}
